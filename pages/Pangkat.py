4cfed8c1e7f8983de9d7b0a0325e2775a3409bb8
FadilaTriAfriani2011521011 <2011521011_fadila@student.unand.ac.id>
2023-02-08 11:39:58 +0700

    cbpangkat

--- pages/Pangkat.py
+++ pages/Pangkat.py
@@ -7,6 +7,9 @@ import plotly.subplots as sp
 import subprocess
 import sys
 from streamlit_option_menu import option_menu
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title='Data Pangkat', page_icon=':bar_chart:', layout='wide')
 st.title('Data Pangkat Pegawai BPS se-Provinsi Sumatera Barat')
@@ -28,7 +31,7 @@ sheet_name3 = 'Pangkat'
 # ===== Pangkat =====
 df = pd.read_excel(excel_file,
                    sheet_name=sheet_name3,
-                   usecols='A:P',
+                   usecols='A:N',
                    header=0)
 df_participants = pd.read_excel(excel_file,
                                 sheet_name= sheet_name3,
@@ -59,39 +62,138 @@ if(option == 'All'):
         if(daerah_selection3):
             filter_Pangkat_2021 = df[df['Unit Kerja'].isin(daerah_selection3)]
             c1, c2 = st.columns(2)
+            c3, c4 = st.columns(2)
+            c5, c6 = st.columns(2)
             with c1:
                 plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
                         filter_Pangkat_2021['II 2021'].values)
                 plt.xlabel("Unit Kerja")
                 plt.ylabel("Value")
-                plt.title("Sarjana 2021")
+                plt.title("Golongan II 2021")
                 plt.xticks(rotation=90)
                 st.pyplot()
                 st.set_option('deprecation.showPyplotGlobalUse', False)
             with c2:
-                plt.bar(filter_Pendidikan_2021['Unit Kerja'].values,
-                        filter_Pendidikan_2021['Sarjana 2022'].values)
+                plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
+                        filter_Pangkat_2021['II 2022'].values)
                 plt.xlabel("Unit Kerja")
                 plt.ylabel("Value")
-                plt.title("Sarjana 2022")
+                plt.title("Golongan II 2022")
                 plt.xticks(rotation=90)
                 st.pyplot()
                 st.set_option('deprecation.showPyplotGlobalUse', False)
+            with c3:
+                plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
+                        filter_Pangkat_2021['III 2021'].values)
+                plt.xlabel("Unit Kerja")
+                plt.ylabel("Value")
+                plt.title("Golongan III 2021")
+                plt.xticks(rotation=90)
+                st.pyplot()
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+            with c4:
+                plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
+                        filter_Pangkat_2021['III 2022'].values)
+                plt.xlabel("Unit Kerja")
+                plt.ylabel("Value")
+                plt.title("Golongan III 2022")
+                plt.xticks(rotation=90)
+                st.pyplot()
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+            with c5:
+                plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
+                        filter_Pangkat_2021['IV 2021'].values)
+                plt.xlabel("Unit Kerja")
+                plt.ylabel("Value")
+                plt.title("Golongan IV 2021")
+                plt.xticks(rotation=90)
+                st.pyplot()
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+            with c6:
+                plt.bar(filter_Pangkat_2021['Unit Kerja'].values,
+                        filter_Pangkat_2021['IV 2022'].values)
+                plt.xlabel("Unit Kerja")
+                plt.ylabel("Value")
+                plt.title("Golongan IV 2022")
+                plt.xticks(rotation=90)
+                st.pyplot()
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+    
+elif(option == 'Daerah'):
+    unit_kerja = df['Unit Kerja'].unique().tolist()
+    unit_kerja_selection = st.selectbox('Pilih Daerah : ', unit_kerja)
+    m1, m2, m3= st.columns((1, 1, 1))
+    todf = pd.read_excel('pegawai.xlsx', sheet_name='Pangkat')
+    to = todf[(todf['Unit Kerja'] == unit_kerja_selection)]
 
+    # ===== Delta Section =====
+    perII2 = int(to['II 2022']) - int(to['II 2021'])
+    perIII2 = int(to['III 2022']) - int(to['III 2021'])
+    perIV2 = int(to['IV 2022']) - int(to['IV 2021'])
+   
+    # ===== Comparation =====
+    totalII2022 = df['II 2022'].sum()
+    totalIII2022 = df['III 2022'].sum()
+    totalIV2022 = df['IV 2022'].sum()
+    
 
+    with m1:
+        m1.metric(label ='Jumlah Gol II 2022',value = int(to['II 2022']), delta= perII2)
+        m1.metric(label ='Jumlah Gol II 2022 se-Provinsi Sumbar',value = totalII2022)
+    with m2:
+         m2.metric(label ='Jumlah Gol III 2022',value = int(to['III 2022']), delta= perIII2)
+         m2.metric(label ='Jumlah Gol III 2022 se-Provinsi Sumbar',value = totalIII2022)
+    with m3:
+         m3.metric(label ='Jumlah Gol IV 2022',value = int(to['IV 2022']), delta= perIV2)
+         m3.metric(label ='Jumlah Gol IV se-Provinsi Sumbar',value = totalIV2022)
+   
+    
+    # ----- MANIPULATION for Man -----
+    # ===== List for Values =====
+    single_row_df = to[0:1]
+    listValuesPangkat = []
+    list_from_df_dua2 = single_row_df.values.tolist()[0]
+    for i in range(2, 4):
+        listValuesPangkat.append(list_from_df_dua2[i])
+    # ----- END OF MANIPULATION -----
+    # ===== Show Bar Chart =====
+    unit_kerja_Pangkat = pd.DataFrame({
+        'Value': listValuesPangkat
+    }, index=['2021', '2022'])
+    st.subheader("Line Golongan II")
+    st.line_chart(unit_kerja_Pangkat,  y='Value', use_container_width=True)
+    
+    # ----- MANIPULATION for Man -----
+    # ===== List for Values =====
+    single_row_df = to[0:1]
+    listValuesPangkat = []
+    list_from_df_tiga2 = single_row_df.values.tolist()[0]
+    for i in range(5, 7):
+        listValuesPangkat.append(list_from_df_tiga2[i])
+    # ----- END OF MANIPULATION -----
+    # ===== Show Bar Chart =====
+    unit_kerja_Pangkat = pd.DataFrame({
+        'Value': listValuesPangkat
+    }, index=['2021', '2022'])
+    st.subheader("Line Golongan III")
+    st.line_chart(unit_kerja_Pangkat,  y='Value', use_container_width=True)
+    
+     # ----- MANIPULATION for Woman -----
+    # ===== List for Values =====
+    single_row_df = to[0:1]
+    listValuesPangkat = []
+    list_from_df_empat2 = single_row_df.values.tolist()[0]
+    for i in range(8, 10):
+        listValuesPangkat.append(list_from_df_empat2[i])
+    # ----- END OF MANIPULATION -----
+    # ===== Show Bar Chart =====
+    unit_kerja_Pangkat = pd.DataFrame({
+        'Value': listValuesPangkat
+    }, index=['2021', '2022'])
+    st.subheader("Line Golongan IV")
+    st.line_chart(unit_kerja_Pangkat,  y='Value', use_container_width=True)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
@@ -115,6 +217,3 @@ if(option == 'All'):
 #     m3.metric(label ='Golongan IV/c',value = int(to['IV/c']))
 #     m3.metric(label ='Golongan IV/d',value = int(to['IV/d']))
 #     m4.metric(label ='Total',value = int(to['Total']))
-
-
-
