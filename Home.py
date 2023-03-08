9c49f3baf46dd54156698ab1464b6b219025b7d4
FadilaTriAfriani2011521011 <2011521011_fadila@student.unand.ac.id>
2023-03-08 21:41:58 +0700

    test

--- Home.py
+++ Home.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 st.set_page_config(page_title='Home', page_icon=':bar_chart:', layout='wide')
 
-# Setting
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+# # Setting
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # Layout
 st.title('DIVISI SDM DAN HUKUM BPS PROVINSI SUMATERA BARAT ') 
@@ -18,14 +18,21 @@ st.title('⭐ Overview')
 st.write('Badan Pusat Statistik adalah Lembaga Pemerintah Non Kementerian yang bertanggung jawab langsung kepada Presiden. Pada perusahaan BPS dibagi menjadi beberapa divisi/bidang. Salah satu divisi di Badan Pusat Statistik adalah Divisi Sumber Daya Manusia (SDM) dan Hukum. Dimana Divisi SDM dan Hukum ini bertamggung jawab terhadap urusan kepegawaian dan aturan bagi pegawai di Badan Pusat Statistik.')
 
 
-st.write('Untuk memberikan informasi yang lebih interaktif bagi Divisi SDM dan Hukum di Badan Pusat Statistik, diajukan sebuah pemecahan masalah berupa rancangan Dashboard. Dashboard merupakan sebuah tampilan visual dari informasi penting yang dibutuhkan untuk tercapainya suatu tujuan, digabungkan dan diatur pada sebuah layar yang menghasilkan informasi yang dibutuhkan dan didapat secara sekilas. Tampilan visual disini didefinisikan bahwa penyajian informasi harus dirancang sebaik mungkin, sehingga memudahkan pengguna untuk menangkap informasi secara cepat dan memahami maknanya secara benar.')
+st.write('Divisi Sumber Daya Manusia dan Hukum bertugas dalam pengelolaan karyawan yang meliputi, penetapan kenaikan pangkat untuk Pegawai Negeri Sipil (PNS) golongan IV/b ke bawah, penetapan pensiun, pemindahan PNS, pencantuman gelar PNS, serta melihat angka beban kerja atau jumlah PNS yang berada di setiap daerah.') 
+st.write('Adapun fungsi Divisi SDM dan Hukum ini meliputi : ')
+st.write(' 1. Penerimaan berkas Calon Pegawai Negeri Sipil (CPNS)')
+st.write(' 2. Penempatan, pengangkatan, pengurusan berkas untuk pensiun')
+st.write(' 3. Memperbaharui jabatan karyawan pada website BKN (Badan Kepegawaian Negara)')
+st.write(' 4. Mutase/pemindahan PNS antar divisi, daerah, atau antar instansi')
 
-st.title('⭐ Metode  ')
-st.write('Terdapat beberapa metode pembangunan dashboard. Dalam pembangunan dashboard untuk Divisi SDM dan Hukum, digunakan metode pureshare. Metode pureshare dikembangkan oleh vendor pureshare untuk memberi fasilitas terhadap proyek yang berhubungan dengan upaya pengelolaan dan pengukuran kinerja organisasi, termasuk pembangunan dashboard. Pembangunan dashboard dirancang supaya selaras dengan kebutuhan teknologi dan tujuan bisnisnya. Metode pureshare menggunakan dua pendekatan yang biasa disebut top-down desaign dan bottom-up implementation.  Metodologi melibatkan pengguna secara aktif dalam proyek pembangunan dashboard secara cepat. Proses ini terbukti menurunkan tingkat resiko proyek dengan melibatkan enduser dalam pembuatan dashboard serta mempercepat dalam penerapannya.')
-st.write('Tahapan-tahapan dalam metode pureshare :')
-st.write('a. Perencanaan dan desain')
-st.write('b. Review sistem dan data')
-st.write('c. Perancangan prototype')
-st.write('d. Perbaikan prototype')
-st.write('e. Realese ')
-st.write('f. Perbaikan terus menerus')
+st.write('Untuk memberikan informasi yang lebih interaktif bagi Divisi SDM dan Hukum di Badan Pusat Statistik, diajukan sebuah pemecahan masalah berupa rancangan Dashboard. ')
+st.write('Untuk memberikan informasi yang lebih interaktif bagi Divisi SDM dan Hukum di Badan Pusat Statistik, diajukan sebuah pemecahan masalah berupa rancangan Dashboard. Dashboard merupakan sebuah tampilan visual dari informasi penting yang dibutuhkan untuk tercapainya suatu tujuan, digabungkan dan diatur pada sebuah layar yang menghasilkan informasi yang dibutuhkan dan didapat secara sekilas. Tampilan visual disini didefinisikan bahwa penyajian informasi harus dirancang sebaik mungkin, sehingga memudahkan pengguna untuk menangkap informasi secara cepat dan memahami maknanya secara benar.')
+# st.title('⭐ Metode  ')
+# st.write('Terdapat beberapa metode pembangunan dashboard. Dalam pembangunan dashboard untuk Divisi SDM dan Hukum, digunakan metode pureshare. Metode pureshare dikembangkan oleh vendor pureshare untuk memberi fasilitas terhadap proyek yang berhubungan dengan upaya pengelolaan dan pengukuran kinerja organisasi, termasuk pembangunan dashboard. Pembangunan dashboard dirancang supaya selaras dengan kebutuhan teknologi dan tujuan bisnisnya. Metode pureshare menggunakan dua pendekatan yang biasa disebut top-down desaign dan bottom-up implementation.  Metodologi melibatkan pengguna secara aktif dalam proyek pembangunan dashboard secara cepat. Proses ini terbukti menurunkan tingkat resiko proyek dengan melibatkan enduser dalam pembuatan dashboard serta mempercepat dalam penerapannya.')
+# st.write('Tahapan-tahapan dalam metode pureshare :')
+# st.write('a. Perencanaan dan desain')
+# st.write('b. Review sistem dan data')
+# st.write('c. Perancangan prototype')
+# st.write('d. Perbaikan prototype')
+# st.write('e. Realese ')
+# st.write('f. Perbaikan terus menerus')
